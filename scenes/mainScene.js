e0bef9c014417ee0337a6067c1da402f8d48f57b
pearl.vernon-howe <pearl.vernon-howe@Twin10-L186.corp.twinkl.com>
2024-06-05 10:57:03 +0100

    add moo moo juice map and tilesets

--- scenes/mainScene.js
+++ scenes/mainScene.js
@@ -14,21 +14,33 @@ export default class MainScene extends Scene {
 
     preload() {
         Player.preload(this);
-        this.load.image('tiles', 'assets/images/RPG Nature Tileset.png');
-        this.load.tilemapTiledJSON('map', 'assets/images/map.json');
+        // this.load.image('tiles', 'assets/images/RPG Nature Tileset.png');
+        // this.load.tilemapTiledJSON('map', 'assets/images/map.json');
+        this.load.image('water', 'assets/images/moo-moo-juice/Water.png');
+        this.load.image('grass', 'assets/images/moo-moo-juice/Grass.png');
+        this.load.image('biome', 'assets/images/moo-moo-juice/Basic_Grass_Biom_things.png');
+        this.load.image('dirt', 'assets/images/moo-moo-juice/Tilled_Dirt.png');
+        this.load.tilemapTiledJSON('map', 'assets/images/moo-moo-juice/map.json');
     }
 
     create() {
         const map = this.add.tilemap('map');
-        const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32, 0, 0);
-        const layer1 = map.createLayer('Tile Layer 1', tileset, 0, 0);
-        const layer2 = map.createLayer('Tile Layer 2', tileset, 0, 0);
-        const layer3 = map.createLayer('Tile Layer 3', tileset, 0, 0);
+        // png name + loaded image key + px width + height + x + y
+        const waterTileset = map.addTilesetImage('Water', 'water', 16, 16, 0, 0);
+        const grassTileset = map.addTilesetImage('Grass', 'grass', 16, 16, 0, 0);
+        const biomeTileset = map.addTilesetImage('Basic_Grass_Biom_things', 'biome', 16, 16, 0, 0);
+        const dirtTileset = map.addTilesetImage('Tilled_Dirt', 'dirt', 16, 16, 0, 0);
+        // layer name in map + created tileset + x + y
+        const layer1 = map.createLayer('Water', waterTileset, 0, 0);
+        const layer2 = map.createLayer('Island', grassTileset, 0, 0);
+        const layer3 = map.createLayer('Island Edges', grassTileset, 0, 0);
+        const layer4 = map.createLayer('Nature', biomeTileset, 0, 0);
+        const layer5 = map.createLayer('Tilled Ground', dirtTileset, 0, 0);
 
-        layer1.setCollisionByProperty({ collides: true });
-        layer3.setCollisionByProperty({ collides: true });
-        this.matter.world.convertTilemapLayer(layer1);
-        this.matter.world.convertTilemapLayer(layer3);
+        // layer1.setCollisionByProperty({ collides: true });
+        // layer3.setCollisionByProperty({ collides: true });
+        // this.matter.world.convertTilemapLayer(layer1);
+        // this.matter.world.convertTilemapLayer(layer3);
 
         this.inputKeys = this.input.keyboard.addKeys({
             up: Input.Keyboard.KeyCodes.W,
